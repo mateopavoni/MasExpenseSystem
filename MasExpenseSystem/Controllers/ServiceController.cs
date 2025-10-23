3ca244e2b35616f304b7255e1f62927f4fbc8c9b
Mateo Pavoni <mateopavoni905@gmail.com>
2025-10-23 00:01:49 -0300

    feat: agregue el controlador para Service y cree la vista Index del controlador con logica para mostrar los Service de la bbdd en una table

---
+++ MasExpenseSystem/Controllers/ServiceController.cs
@@ -0,0 +1,16 @@
+﻿using MasExpenseSystem.Context;
+using MasExpenseSystem.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasExpenseSystem.Controllers
+{
+    public class ServiceController(AppDbContext _dbContext) : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            List<Service> services = _dbContext.Services.ToList();
+            return View(services);
+        }
+    }
+}
